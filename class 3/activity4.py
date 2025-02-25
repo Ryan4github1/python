6d30500f2de8ba956fee156b4c0a041290281ba5
Ryan4github1 <ryan.singapur04@gmail.com>
2025-02-25 20:59:34 +1100

    class3

---
+++ class 3/activity4.py
@@ -0,0 +1,13 @@
+str="football"
+print(str[4])
+print(str[2:6:1])
+print(str[:4:])
+print("reverse:", str[::-1])
+print("no letters=",len(str))
+print(str.upper())
+print(str.lower())
+print(str.capitalize())
+a="hello"
+b="hi"
+print(a+b)
+print(a*3)
