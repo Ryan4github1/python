25a98b8aa48d0d804708cec966063191995d04f1
Ryan4github1 <ryan.singapur04@gmail.com>
2025-05-08 19:53:50 +1000

    class 22

---
+++ class 22/activity1.py
@@ -0,0 +1,12 @@
+my_set = {1, 2, 3 , 3, 4 , 5 , 4 }
+print(my_set)
+my_set = {1.0, "Hi" ,(1, 2, 3, 3, 4, 5, )}
+print(my_set)
+my_set = set([1,2,3])
+print(my_set,"\n")
+num_set=set([0,1,3,5,])
+print("original set:")
+print(num_set)
+num_set.pop()
+print("after removing the removing the element from the said set")
+print(num_set,"\n")
---
+++ class 22/activity2.py
@@ -0,0 +1,8 @@
+setx={'green','blue'}
+sety={'yellow','blue'}
+print("original set elements:")
+print(setx)
+print(sety)
+print("/nintersections of two said sets:")
+setz=setx.intersection(sety)
+print(setz)
