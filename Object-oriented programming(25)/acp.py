b82a3b12db1d835465def789cb447833722cde03
Ryan4github1 <ryan.singapur04@gmail.com>
2025-05-22 20:05:21 +1000

    Object-oriented programming(25)

--- Object-oriented programming(25)/acp.py
+++ Object-oriented programming(25)/acp.py
@@ -1,11 +1,11 @@
 class parrot:
-    species="parrot"
+    species="dog"
     def __init__(self,name,age):
         self.nam=name
         self.a=age
-bluobj=parrot("blu",10)
-wooobj=parrot("woo",25)
-print(bluobj.nam," is ", bluobj.a, "years old") 
-print(wooobj.nam," is ", wooobj.a, "years old")
-print(bluobj.nam," is ",bluobj.species)
-print(wooobj.nam," is ",wooobj.species)
+bullobj=dog("bull",10)
+beagleobj=dog("beagle",25)
+print(bullobj.nam," is ", bullobj.a, "years old") 
+print(beagleobj.nam," is ", beagleobj.a, "years old")
+print(beagleobj.nam," is ",beagleobj.species)
+print(bullobj.nam," is ",bullobj.species)
