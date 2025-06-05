620937904fbff50924546534e1dfde416295c05e
Ryan4github1 <ryan.singapur04@gmail.com>
2025-06-05 19:58:32 +1000

    Encapsulation and Special Functions(28)

---
+++ Encapsulation and Special Functions(28)/activity1.py
@@ -0,0 +1,10 @@
+class myclass:
+    __privateVar = 27
+    def __privMeth(self):
+        print("I'm inside class myclass")
+    def hello(self):
+        print("Private Variable value: ",myclass.__privateVar)
+
+foo = myclass()
+foo.hello()
+foo.__privMeth
