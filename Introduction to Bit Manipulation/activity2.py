2e475ae9fa17a3481e4a496707b98372b2f16d1f
Ryan4github1 <ryan.singapur04@gmail.com>
2025-08-21 20:07:00 +1000

    Introduction to Bit Manipulation

---
+++ Introduction to Bit Manipulation/activity2.py
@@ -0,0 +1,3 @@
+a=20
+print(a<<1)#left shift multiplied by by 2
+print(a>>1)#right shift divided by 2
---
+++ Introduction to Bit Manipulation/activity3.py
@@ -0,0 +1,5 @@
+n=int(input("enter a number:"))
+if (n^1==n+1):
+    print("even")
+else:
+    print("odd")
