c45c92e984c73fa41ed78d4a423eaecda3013791
Ryan4github1 <ryan.singapur04@gmail.com>
2025-09-23 20:05:34 +1000

    sorting

---
+++ sorting/acp.py
@@ -0,0 +1,9 @@
+def checklength(a):
+    length = len(a)
+    if length == 0:
+        print("The list is empty")
+    else:
+        print("The length of the list is:", length)
+
+lst = [8, 22, 38, 42, 67]
+checklength(lst)
